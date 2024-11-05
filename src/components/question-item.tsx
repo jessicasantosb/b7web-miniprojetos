f7d5bb4584c8f9155339544f73d48ee3fb8ad31b
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-05 18:40:06 -0300

    feat: change style when answer is selected

--- src/components/question-item.tsx
+++ src/components/question-item.tsx
@@ -5,11 +5,18 @@ export function QuestionItem({ question, count, onAnswer }: QuestionItemProps) {
   const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
 
   const checkQuestion = (key: number) => {
-    if (key === null) {
+    console.log(key);
+
+    if (key !== null) {
       setSelectedAnswer(key);
       onAnswer(key);
     }
   };
+
+  const alreadySelected = selectedAnswer !== null;
+  const rigthAnswer = alreadySelected && selectedAnswer === question.answer;
+  const wrongAnswer = alreadySelected && selectedAnswer !== question.answer;
+
   return (
     <div className="pb-6">
       <h3 className="pb-2">
@@ -18,17 +25,22 @@ export function QuestionItem({ question, count, onAnswer }: QuestionItemProps) {
 
       {question.options.map((option, key) => {
         return (
-          <div key={key}>
-            <label className="flex gap-2">
-              <input
-                type="radio"
-                name={question.question}
-                value={option}
-                onChange={() => checkQuestion(key)}
-              />
-              {option}
-            </label>
-          </div>
+          <label
+            key={key}
+            className={`p-2 mb-2 flex gap-2 cursor-pointer bg-blue-100 hover:opacity-70
+              ${alreadySelected && "cursor-auto"}
+              ${rigthAnswer && selectedAnswer === key && "bg-green-100"}
+              ${wrongAnswer && selectedAnswer === key && "bg-red-100"}
+              `}
+          >
+            <input
+              type="radio"
+              name={question.question}
+              value={option}
+              onChange={() => checkQuestion(key)}
+            />
+            {option}
+          </label>
         );
       })}
     </div>
